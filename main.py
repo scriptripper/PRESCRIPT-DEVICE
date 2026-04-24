38fe7d7c6795d988408ff2912c32436588347ccb
Leo <99474078+scriptripper@users.noreply.github.com>
2026-04-24 13:24:06 +1000

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ frame.place(x=0,y=90)
 
 glitch_chars = list("█▒░ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
-choices = ["JJS","BT","BDGR","8BA","D2"]
+choices = ["LCB","LCCB","8BA","D2","JJS","BT","BDGR"]
 
 prescript_jjs1 = [
     f"_After {random.randint(1, 20)} seconds, ",
@@ -45,16 +45,13 @@ prescript_jjs1 = [
     "_Without retreating, ",
     "_Without using M1s, ",
     "_Without using special, ",
-    "_Without using move 1, ",
-    "_Without using move 2, ",
-    "_Without using move 3, ",
-    "_Without using move 4, ",
+    f"_Without using move {random.randint(1, 4)}, ",
     "_Without activating awakening, ",
     "_Without moving, "
 
 ]
 prescript_jjs2 = [
-    "kill a player._",
+    "eliminate a player._",
     "jump on a player's head._",
     "use an emote._",
     "seek death._",
@@ -67,10 +64,7 @@ prescript_jjs2 = [
     f"stop attacking for {random.randint(1, 20)} seconds._",
     f"stay alive for {random.randint(1, 20)} seconds._",
     "land special._",
-    "land move 1._",
-    "land move 2._",
-    "land move 3._",
-    "land move 4._",
+    f"land move {random.randint(1, 4)}._",
     "activate awakening._",
     "offer a greeting._",
     "recite the first 5 digits of e._",
@@ -106,55 +100,28 @@ prescript_bt1 = [
     "_Without using items, ",
 ]
 prescript_bt2 = [
-    "perform a base attack on target #1._",
-    "perform a base attack on target #2._",
-    "perform a base attack on target #3._",
-    "perform a base attack on target #4._",
+    f"perform a base attack on target #{random.randint(1, 4)}._",
     "perform a base attack._",
-    "use dynamite on target #1._",
-    "use dynamite on target #2._",
-    "use dynamite on target #3._",
-    "use dynamite on target #4._",
+    f"use dynamite on target #{random.randint(1, 4)}._",
     "use dynamite._",
     "use ghost potion._",
-    "use shovel on target #1._",
-    "use shovel on target #2._",
-    "use shovel on target #3._",
-    "use shovel on target #4._",
+    f"use shovel on target #{random.randint(1, 4)}._",
     "use shovel._",
     "evade an attack._",
     "retreat._",
     "defend._",
     "focus._",
-    "use an item on player #1._",
-    "use an item on player #2._",
-    "use an item on player #3._",
-    "use an item on player #4._",
+    f"use an item on player #{random.randint(1, 4)}._",
     "use an item on any player._",
     "heal any player._",
-    "heal player #1._",
-    "heal player #2._",
-    "heal player #3._",
-    "heal player #4._",
+    f"heal player #{random.randint(1, 4)}._",
     "use a strategy option._",
     "use any special action._",
     "use any special._",
-    "use special #1._",
-    "use special #2._",
-    "use special #3._",
-    "use special #4._",
-    "next action must target player #1._",
-    "next action must target player #2._",
-    "next action must target player #3._",
-    "next action must target player #4._",
-    "next action must target enemy #1._",
-    "next action must target enemy #2._",
-    "next action must target enemy #3._",
-    "next action must target enemy #4._",
-    "kill enemy #1._",
-    "kill enemy #2._",
-    "kill enemy #3._",
-    "kill enemy #4._",
+    f"use special #{random.randint(1, 4)}._",
+    f"next action must target player #{random.randint(1, 4)}._",
+    f"next action must target enemy #{random.randint(1, 4)}._",
+    f"eliminate enemy #{random.randint(1, 4)}._",
     "cease all actions for the turn._",
     "offer a greeting._",
     "bid farewell._",
@@ -197,7 +164,7 @@ prescript_bdgr1 = [
     "_Without using ocular prowess, "
 ]
 prescript_bdgr2 = [
-    "kill a player._",
+    "eliminate a player._",
     "jump on a player's head._",
     "land a headshot._",
     "seek death._",
@@ -210,8 +177,7 @@ prescript_bdgr2 = [
     f"stop attacking for {random.randint(1, 20)} seconds._",
     f"stay alive for {random.randint(1, 20)} seconds._",
     "eat/drink._",
-    "empty chamber of gun #1._",
-    "empty chamber of gun #2._",
+    f"empty chamber of gun #{random.randint(1, 2)}._",
     "reload._",
     "offer a greeting._",
     "recite the first 5 digits of e._",
@@ -237,6 +203,130 @@ prescript_bdgr2 = [
     "catch a fish._",
     "surrender._"
 ]
+prescript_lccb = [
+    "_Use all identities from The Index._",
+    "_Use only burn identities._",
+    "_Use only bleed identities._",
+    "_Use only tremor identities._",
+    "_Use only rupture identities._",
+    "_Use only sinking identities._",
+    "_Use only poise identities._",
+    "_Use only charge identities._",
+    "_Use all bloodfiend identities._",
+    "_Use all identities from The Kurokumo Clan._",
+    "_Use all identities from Blade Lineage._",
+    "_Use all identities from Wuthering Heights._",
+    "_Use only LCB Sinner identities._",
+    "_Use all identities from Lobotomy Corporation._",
+    "_Use all identities from N Corp._",
+    "_Use all identities from The Fingers._",
+    "_Use all identities from The Middle._",
+    "_Use all identities from Walpurgis._",
+    "_Use all identities from R Corp._",
+    "_Use all identities from the Dieci Association._",
+    "_Use all identities from H Corp._",
+    "_Use all identities from The Ring._",
+    "_Use all identities from W Corp._",
+    "_Use only bad ending identities._",
+    "_Use all identities from the Zwei Association._",
+    "_Use all identities from the Great Lake._",
+    f"_Use only season {random.randint(1, 7)} identities._"
+
+]
+prescript_lc1 = [
+    f"_After winning a clash with unit #{random.randint(1, 7)}, ",
+    f"_After losing a clash with unit #{random.randint(1, 7)}, ",
+    "_After winning any clash, ",
+    "_After losing any clash, ",
+    f"_After {random.randint(2, 5)} turns, ",
+    "_Next turn, ",
+    f"_After rolling heads with unit #{random.randint(1, 7)}, ",
+    f"_After rolling tails with unit #{random.randint(1, 7)}, ",
+    "_After a unit dies, ",
+    "_After landing a hit, ",
+    "_After using a defense skill, ",
+    "_As soon as possible, ",
+    "_Next turn, ",
+    "_After a unit gets staggered, ",
+    f"_After unit #{random.randint(1, 7)} gets staggered, ",
+    f"_Within the next {random.randint(1, 5)} turns, ",
+    "_Without any units getting staggered, ",
+    f"_Without unit #{random.randint(1, 7)} getting staggered, ",
+    "_Without any units dying, ",
+    f"_Without unit unit #{random.randint(1, 7)} dying, ",
+    f"_Without unit #{random.randint(1, 7)} attacking, ",
+    "_Without restarting the encounter, ",
+    "_Next encounter, ",
+    f"_Without using any skill {random.randint(1, 3)}s, ",
+    f"_Without unit #{random.randint(1, 7)} using skill {random.randint(1, 3)}, ",
+    "_Without targeting an enemy's core, ",
+    "_While only targeting an enemy's core, ",
+    "_Without rolling tails, ",
+    f"_Without unit #{random.randint(1, 7)} rolling heads, ",
+    f"_Without unit #{random.randint(1, 7)} rolling tails, ",
+    "_Without using any E.G.O skills, ",
+    f"_Without using unit #{random.randint(1, 7)}'s E.G.O skills, ",
+    "_Without using a Zayin E.G.O, ",
+    "_Without using a Teth E.G.O, ",
+    "_Without using a He E.G.O, ",
+    "_Without using a Waw E.G.O, ",
+    f"_Without using unit #{random.randint(1, 7)}'s Zayin E.G.O, ",
+    f"_Without using unit #{random.randint(1, 7)}'s Teth E.G.O, ",
+    f"_Without using unit #{random.randint(1, 7)}'s He E.G.O, ",
+    f"_Without using unit #{random.randint(1, 7)}'s Waw E.G.O, ",
+]
+prescript_lc2 = [
+    f"use unit #{random.randint(1, 7)}'s skill {random.randint(1, 3)}._",
+    f"use unit #{random.randint(1, 7)}'s defense skill._",
+    "eliminate an enemy._",
+    "stagger an enemy._",
+    "eliminate any unit._",
+    "stagger any unit._",
+    "eliminate any ally._",
+    "stagger any ally._",
+    f"eliminate unit #{random.randint(1, 7)}._",
+    f"stagger unit #{random.randint(1, 7)}._",
+    "use a Zayin E.G.O._",
+    "use a Teth E.G.O._",
+    "use a He E.G.O._",
+    "use a Waw E.G.O._",
+    f"use unit #{random.randint(1, 7)}'s Zayin E.G.O._",
+    f"use unit #{random.randint(1, 7)}'s Teth E.G.O._",
+    f"use unit #{random.randint(1, 7)}'s He E.G.O._",
+    f"use unit #{random.randint(1, 7)}'s Waw E.G.O._",
+    "stagger or eliminate an enemy._",
+    "stagger or eliminate all enemies._",
+    "eliminate all enemies._",
+    f"form a {random.randint(2, 7)}x sin resonance._",
+    f"form a {random.randint(2, 4)}x sin resonance._",
+    "form any sin resonance._",
+    "form an absolute resonance._",
+    f"form a {random.randint(2, 7)}x absolute resonance._",
+    f"form a {random.randint(2, 4)}x absolute resonance._",
+    "repeat last prescript command._",
+    "target the slowest enemy with any unit._",
+    f"target the slowest enemy with unit #{random.randint(1, 7)}._",
+    "target the fastest enemy with any unit._",
+    f"target the fastest enemy with unit #{random.randint(1, 7)}._",
+    "target the enemy with the least HP with any unit._",
+    f"target the enemy with the least HP with unit #{random.randint(1, 7)}._",
+    "target the enemy with the most HP with any unit._",
+    f"target the enemy with the most HP with unit #{random.randint(1, 7)}._",
+    "target the slowest enemy with the unit with the least HP._",
+    "target the slowest enemy with the unit with the most HP._",
+    "target the fastest enemy with the unit with the least HP._",
+    "target the fastest enemy with the unit with the most HP._",
+    "target the enemy with the least HP with the unit with the least HP._",
+    "target the enemy with the least HP with the unit with the most HP._",
+    "target the enemy with the most HP with the unit with the least HP._",
+    "target the enemy with the most HP with the unit with the most HP._",
+    "perform an unopposed attack against an enemy._",
+    f"perform an unopposed attack against an enemy with unit {random.randint(1, 7)}._",
+    "use the winrate button._",
+    "use the damage button._"
+
+]
+
 prescript_d2 = [
     "_Proceed._",
     "_Cease._"
@@ -251,11 +341,11 @@ prescript_8ba = [
     "_Insuficient data. Await further instruction._",
     "_The path is unclear._",
     "_You are being tested. Choose carefully._",
-    "_Compliance increases survival probability._",
+    "_Compliance increases odds._",
     "_Noncompliance will be recorded._",
     "_The answer is yes._",
     "_No. Do not ask again._",
-    "_A better question exists._",
+    "_There is a better question._",
     "_This action aligns with the prescript's will._",
     "_No comment._",
     "_Delay. Timing is not yet correct._",
@@ -298,6 +388,9 @@ def clicked():
     elif cv == "BDGR":
         delay = random.randint(1000, 120000)
         glitch_text(intermission)
+    elif cv == "LCB" or "LCCB":
+        delay = random.randint(1000, 15000)
+        glitch_text(intermission)
     elif cv == "BT":
         delay = random.randint(1000, 30000)
         glitch_text(intermission)
@@ -322,6 +415,13 @@ def initiate_prescript():
         result1 = random.choice(prescript_bdgr1)
         result2 = random.choice(prescript_bdgr2)
         prescript = f"{result1}{result2}"
+    elif cv == "LCB":
+        result1 = random.choice(prescript_lc1)
+        result2 = random.choice(prescript_lc2)
+        prescript = f"{result1}{result2}"
+    elif cv == "LCCB":
+        result = random.choice(prescript_lccb)
+        prescript = f"{result}"
     elif cv == "8BA":
         result = random.choice(prescript_8ba)
         prescript = f"{result}"
@@ -342,7 +442,7 @@ btn = Button(frame, text = "[+]",
 btn.pack(side="left")
 
 context = StringVar()
-context.set("JJS")
+context.set("LCB")
 
 drp = OptionMenu(frame, context, *choices)
 drp.config(font=("Perfect DOS VGA 437", 25), 
